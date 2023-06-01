c4f062a11ed9241b86606cb005f88cf1822d85e6
Stephen Pederson <stephen.pederson.au@gmail.com>
2023-06-01 15:10:47 +0800

    Partial run. Need to debug some scripts

--- workflow/scripts/create_site_yaml.R
+++ workflow/scripts/create_site_yaml.R
@@ -6,7 +6,17 @@
 #' Uses the information provided in the config file to update the navigation
 #' bar
 #'
-#'
+
+#' Set the .libPaths() first in case there are conflicts in a conda environment
+conda_pre <- system2("echo", "$CONDA_PREFIX", stdout = TRUE)
+if (conda_pre != ""){
+  conda_lib_path <- file.path(conda_pre, "lib", "R", "library")
+  if (!dir.exists(conda_lib_path)) conda_lib_path <- NULL
+  prev_paths <- .libPaths()
+  paths_to_set <- unique(c(conda_lib_path, prev_paths))
+  .libPaths(paths_to_set)
+}
+
 library(tidyverse)
 library(yaml)
 library(glue)
