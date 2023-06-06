791281176ebd4a25b2a95144e57593165243bbc1
Stevie Pederson <8252496+smped@users.noreply.github.com>
2023-06-06 21:06:55 +0930

    Update make_greylist.R

--- workflow/scripts/make_greylist.R
+++ workflow/scripts/make_greylist.R
@@ -17,7 +17,7 @@ bed <- here::here(args[[4]])
 
 ## Example code for running interactively
 # bam <- here::here("data/deduplicated/SRR8315192.sorted.bam")
-# chrom_sizes <- here::here("output/annotations.chrom.sizes")
+# chrom_sizes <- here::here("output/annotations/chrom.sizes")
 # genome <- "GRCh37"
 # bed <- here::here("output/macs2/SRR8315192/SRR8315192_greylist.bed")
 
